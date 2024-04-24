75fdee2a64297181451a45271fbbe30c727e09d1
JohannFd <samrikov.denis@gmail.com>
2024-04-24 21:19:01 +0500

    Make branch a recursive function

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const height = 400;
-ctx.strokeStyle = 'white';
-ctx.lineWidth = 3;
-ctx.translate(0.5 * height, height)
 
-function branch(len){
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, -len);
-    ctx.stroke();
+var angle = 0;
+var slider;
+
+function setup() {
+  createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
-branch(300)
+function draw() {
+  background(51);
+  angle = slider.value();
+  stroke(255);
+  translate(200, height);
+  branch(120);
+}
+
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > 4) {
+    push();
+    rotate(angle);
+    branch(len * 0.67);
+    pop();
+    push();
+    rotate(-angle);
+    branch(len * 0.67);
+    pop();
+  }
+
+  //line(0, 0, 0, -len * 0.67);
+}
